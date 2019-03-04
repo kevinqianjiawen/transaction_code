01e4b4923e98277b38d36c52bb746d15eb42cb92
cao <1642377@qq.com>
2019-03-04 11:30:20 +0800

    fun_chk_curr_date

--- function/fun_chk_curr_date.java
+++ function/fun_chk_curr_date.java
@@ -1,3 +1,10 @@
+/**
+ * 说明：当组件失去焦点后触发该事件
+ *
+ * @param ... 用户自定义参数值
+ * @throws ParseException
+ * @author cao
+ */
 public void fun_chk_curr_date(String...args)throws ParseException{
         logger.debug("进入OnExit事件处理，args["+args+"]");
         // 获取前台输入时间
@@ -10,5 +17,8 @@ public void fun_chk_curr_date(String...args)throws ParseException{
         System.out.print("时间不正确");
         JOptionPane.showMessageDialog(null,"日期必须小于或等于当前工作日","消息提示",JOptionPane.WARNING_MESSAGE);
         fpluf8_signon_date.setFocus(true);
+        }else{
+
         }
+
         }
