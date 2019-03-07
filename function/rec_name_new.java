098efe05cacb8f50d30ef5c706dba69e9e39964d
cao <1642377@qq.com>
2019-03-07 11:52:36 +0800

    cao

--- function/rec_name_new.java
+++ function/rec_name_new.java
@@ -4,6 +4,7 @@
  * @param vertifyValue
  * 需要验证的数据
  * @param ... 用户自定义参数值 参数 string var_name
+ * @author cao
  */
 public boolean rec_name_new(String vertifyValue,String...args){
         logger.debug("进入OnVailidate事件处理，args["+args+"]");
